36643eba89a7364b1a50a62788cca1d07da809d9
Bruce Chan <bruce20219@gmail.com>
2019-11-30 23:37:36 +0800

    Taiwan

---
+++ main.py
@@ -0,0 +1,8 @@
+strcountry = input("where are you from?")
+intAge = input("how old are you?")
+intAge = int(intAge)
+if strcountry == "Taiwan":
+	if intAge >= 18:
+		print("You can go test of driving exam")
+	else:
+		print("You cannot go test of driving exam")
